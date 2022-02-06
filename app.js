b29aece667371c2d2983f270d73762de0fab8264
Anton <antondragneel69@gmail.com>
2022-02-07 00:25:39 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//1.C помощью promt спросить как зовут пользователя. Вывести имя пользователя с помощью alert.
+    let name = prompt("What your name?")
+    alert(name);
+//2.Создать 2 переменные. Провести между ними следующие математические операции: сложение, вычитание, умножение, деление и возведение в степень. Результат выполнения каждой операции вывести в alert
+//Addition
+    let a = 16;
+    let b = 8;
+    alert(a + b);
+//Subsraction
+    let a = 16;
+    let b = 8;
+    alert(a - b);
+//Multiplication
+    let a = 16;
+    let b = 8;
+    alert(a * b);
+//Division
+    let a = 16;
+    let b = 8;
+    alert(a / b);
+//Exponentiation
+    let a = 16;
+    let b = 8;
+    alert(a ** b);
